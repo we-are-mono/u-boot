ec9f7b6f9d7660426029212a4fb14493dd0bd0ee
RinkeMono <rinke@mono.si>
2025-08-10 22:02:58 +0200

    add errata fix

--- board/freescale/gateway_dk/gateway_dk.c
+++ board/freescale/gateway_dk/gateway_dk.c
@@ -137,8 +137,26 @@ int fan_init(void)
 	return 0;
 }
 
+#define DCFG_CCSR_PORSR1   0x20140000
+#define RCW_SRC_MASK       0x1FF      /* bits 0–8 */
+#define SOME_ADDR          (0x01570000 + 0x1A8)
+
+void workaround_a008127(void)
+{
+    u32 dat;
+
+    dat = in_le32((void *)DCFG_CCSR_PORSR1);
+    dat &= ~RCW_SRC_MASK;             /* Clear RCW_SRC bits */
+    out_le32((void *)DCFG_CCSR_PORSR1, dat);
+
+    out_le32((void *)SOME_ADDR, 0xFFFFFFFF);
+}
+
 int board_early_init_f(void)
 {
+	// Errata fix 008127
+	workaround_a008127();
+
 	fsl_lsch2_early_init_f();
 
 	return 0;
